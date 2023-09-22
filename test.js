24aaa79c0c9c32418e83beb195cf9657e21d85e7
SeaTurtles123 <93158485+SeaTurtles123@users.noreply.github.com>
2023-09-21 20:05:08 -0400

    test

---
+++ test.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+fs.readFile("./test.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("File read failed:", err);
+    return;
+  }
+  console.log("File data:", jsonString);
+});

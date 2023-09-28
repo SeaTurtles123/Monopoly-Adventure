72a2de29e043feab7824eb97bb88cb7044d78b2a
SeaTurtles123 <93158485+SeaTurtles123@users.noreply.github.com>
2023-09-27 20:34:42 -0400

    Removing tests

--- test.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-fs.readFile("./test.json", "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("File read failed:", err);
-    return;
-  }
-  console.log("File data:", jsonString);
-});
